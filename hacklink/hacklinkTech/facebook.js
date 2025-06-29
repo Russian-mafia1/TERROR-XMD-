88b8540e99e7f00fb13ad5907fdcd130962a048a
Russian-mafia1 <ianokipchirchir@gmail.com>
2025-06-29 17:28:09 +0300

    Update facebook.js

--- hacklink/hacklinkTech/facebook.js
+++ hacklink/hacklinkTech/facebook.js
@@ -80,10 +80,10 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
             },
             interactiveMessage: proto.Message.InteractiveMessage.create({
               body: proto.Message.InteractiveMessage.Body.create({
-                text: `*INCONNU XD FACEBOOK POST DOWNLOADER*\n\n> *TITLE*: ${fbData.title}`
+                text: `*TERROR-XMD- FACEBOOK POST DOWNLOADER*\n\n> *TITLE*: ${fbData.title}`
               }),
               footer: proto.Message.InteractiveMessage.Footer.create({
-                text: "©INCONNU BOY"
+                text: "©HACKLINK TECH.INC"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
                 ...(await prepareWAMessageMedia({ image: { url: fbData.thumbnail } }, { upload: Matrix.waUploadToServer })),
@@ -146,7 +146,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
             content = { 
               video: finalMediaBuffer, 
               mimetype: 'video/mp4', 
-              caption: '> © INCONNU BOY',
+              caption: '> © HACKLINK TECH.INC',
             };
             await Matrix.sendMessage(m.from, content, { quoted: m });
           } else {
